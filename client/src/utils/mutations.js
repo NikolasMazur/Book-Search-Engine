b2270c7c51c27588083a0ecc25c5ab6305537d8d
Nikolas Mazur <kolemazur@gmail.com>
2022-08-10 11:38:32 -0600

    Add ADD_USER, SAVE_BOOK, and REMOVE_BOOK mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,46 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+// Needs to be properly tested.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// Needs to be properly tested.
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+// Needs to be properly tested.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: BookInput!) {
+    removeBook(book: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
