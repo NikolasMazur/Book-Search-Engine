43701519e11e627a697f9c9d867c0f0956b06091
Nikolas Mazur <kolemazur@gmail.com>
2022-08-09 19:14:38 -0600

    Fix typos

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,4 @@
-const typeDefs = request('./typeDefs');
-const resolvers = require('.resolvers');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
