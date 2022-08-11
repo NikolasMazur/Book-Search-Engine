9e52d2921939c2bf050753fb1551a2534944feca
Nikolas Mazur <kolemazur@gmail.com>
2022-08-10 21:33:33 -0600

    Fix bug in mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
-    loginUser(email: $email, password: $password) {
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
@@ -11,7 +11,7 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-// Needs to be properly tested.
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -23,34 +23,43 @@ export const ADD_USER = gql`
     }
   }
 `;
-// Needs to be properly tested.
+
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
       _id
       username
       email
+      bookCount
       savedBooks {
+        # _id
         bookId
         authors
-        description
-        title
         image
         link
+        title
+        description
       }
     }
   }
 `;
-// Needs to be properly tested.
+
 export const REMOVE_BOOK = gql`
-  mutation removeBook($book: BookInput!) {
-    removeBook(book: $book) {
-      bookId
-      authors
-      description
-      title
-      image
-      link
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
     }
   }
 `;
